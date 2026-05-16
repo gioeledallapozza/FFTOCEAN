18d59efeed68118d0b69a91bc5e5589786df24f9
Gioele <dpgioele@gmail.com>
2026-05-16 11:51:48 +0200

    basic setup

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,10 @@
 import { OrbitControls } from '@react-three/drei';
+import Ocean from './Ocean.jsx';
 
 export default function Experience() {
     return <>
         <OrbitControls makeDefault />
 
-        <mesh rotation={[-Math.PI / 2, 0, 0]}>
-            <planeGeometry args={[10, 10, 128, 128]} />
-            <meshBasicMaterial color='#00ffff' wireframe />
-        </mesh>
+        <Ocean />
     </>
 }
