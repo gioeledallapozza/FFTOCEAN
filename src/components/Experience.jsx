a1f23aafb35d38101741e5931a0293660d48b86d
Gioele <dpgioele@gmail.com>
2026-05-16 10:22:07 +0200

    Basic Experience Setup

---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+import { OrbitControls } from '@react-three/drei';
+
+export default function Experience() {
+    return <>
+        <OrbitControls makeDefault />
+
+        <mesh rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[10, 10, 128, 128]} />
+            <meshBasicMaterial color='#00ffff' wireframe />
+        </mesh>
+    </>
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import glsl from 'vite-plugin-glsl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    glsl()
+  ],
+  build: {
+    target: 'esnext',
+  },
+  server: {
+    host: true,
+  }
 })
