24ad8f6e0d874cf2f2ae076bef8791666a0a5cf2
Gioele <dpgioele@gmail.com>
2026-06-04 22:57:29 +0200

    Implemented choppiness

--- src/gpgpu/shaders/timeEvolution/timeevolutionfragment.glsl
+++ src/gpgpu/shaders/timeEvolution/timeevolutionfragment.glsl
@@ -2,6 +2,9 @@ uniform sampler2D uH0Target; //R = REAL, G = IMG, B = OMEGA
 uniform float uResolution;
 uniform float uTime;
 
+uniform float uPatchSize; 
+uniform int uOutputMode; // 0 = Height (Y), 1 = Choppy (X), 2 = Choppy (Z)
+
 varying vec2 vUv;
 
 #include "../includes/complex.glsl"
@@ -9,8 +12,6 @@ varying vec2 vUv;
 void main()
 {
     // Tessendorf formula: H(k,t) = H0(k)e^ikt + H0*(-k)e^-iwt
-
-    
     vec2 h0 = texture2D(uH0Target, vUv).rg; //Wave vector: Get the real and imgaginary 
     vec2 negUv = mod(1.0 - vUv + (1.0 / uResolution), 1.0);
     vec2 h0_minus_k = texture2D(uH0Target, negUv).rg; //Opposite wave vector
@@ -32,5 +33,37 @@ void main()
     // Final resoult describes: energy, direction, phase
     vec2 final_h0 = h0_t_pos + h0_t_neg; //Hermitian symmetry
 
-    gl_FragColor = vec4(final_h0.x, final_h0.y, 0.0, 1.0);
+
+    //CHOPPINESS
+    
+    // Ricalculate k vector in pixel space (centered at zero)
+    vec2 k = vec2(
+        (vUv.x - 0.5) * uResolution * (2.0 * 3.14159265 / uPatchSize),
+        (vUv.y - 0.5) * uResolution * (2.0 * 3.14159265 / uPatchSize)
+    );
+
+    // Normalize k vector
+    float kLength = length(k);
+    if (kLength < 0.00001) kLength = 1.0; 
+    vec2 kNormal = k / kLength;
+
+    // Rotate the height by 90 degrees (multiply by -i) to get the choppy displacement direction
+    // Formula: (a + bi) * -i = b - ai
+    vec2 h_choppy = vec2(final_h0.y, -final_h0.x);
+
+    //ouput based on the selected mode, we need 6 channels in total
+    if (uOutputMode == 0) {
+        //Height Y: R = REAL, G = IMG
+        gl_FragColor = vec4(final_h0.x, final_h0.y, 0.0, 1.0);
+    } 
+    else if (uOutputMode == 1) {
+        // Choppy X: R = REAL, G = IMG
+        vec2 dx = h_choppy * kNormal.x;
+        gl_FragColor = vec4(dx, 0.0, 1.0);
+    } 
+    else if (uOutputMode == 2) {
+        // Choppy Z: R = REAL, G = IMG
+        vec2 dz = h_choppy * kNormal.y;
+        gl_FragColor = vec4(dz, 0.0, 1.0);
+    }
 }
